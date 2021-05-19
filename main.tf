2f2fff894204929ff01cd0647ad7566c22fecabb
himanshuupadhyay101 <70565558+himanshuupadhyay101@users.noreply.github.com>
2021-05-19 16:41:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "githubactions-demo"
+    organization = "github-actions"
 
     workspaces {
       name = "gh-actions-demo"
